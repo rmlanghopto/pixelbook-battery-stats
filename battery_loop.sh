11e03bc65c14c98d9caf1220adde61f88d85d2dc
Ryan L <56228006+rmlanghopto@users.noreply.github.com>
2023-07-05 10:46:25 -0400

    Update battery_loop.sh

--- battery_loop.sh
+++ battery_loop.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#In order to send notifications on an interval (e.g., 300 seconds) whenever you start crosh, edit your ~/.bashrc to include code referencing this script.
+#For example:
+# sh /usr/local/bin/battery_loop.sh &
 
 while true
 do  
